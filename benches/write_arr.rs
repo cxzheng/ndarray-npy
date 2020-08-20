4ffe619a07b3ae011f6086414340a8386ab11a9c
Changxi Zheng <cxz@cs.columbia.edu>
2020-08-19 21:14:41 -0400

    added write_arr.rs to meaure the file output perf

---
+++ benches/write_arr.rs
@@ -0,0 +1,30 @@
+use criterion::{criterion_group, criterion_main, Criterion, BenchmarkId}; //, black_box};
+use ndarray_npy::WriteNpyExt;
+use std::fs::File;
+
+fn write_vec(c: &mut Criterion) {
+    let mut group = c.benchmark_group("write_vec");
+    let sizes = [500_usize, 1000, 2000, 4000, 8000];
+    let mut vecs = Vec::with_capacity(sizes.len());
+    for s in &sizes {
+        let mut arr = vec![0_f64; *s];
+        for (i, v) in arr.iter_mut().enumerate() {
+            *v = i as f64;
+        }
+        vecs.push(arr);
+    }
+
+    for i in 0..sizes.len() {
+        group.bench_with_input(BenchmarkId::from_parameter(sizes[i]), &i, |b, &i| {
+            b.iter(|| {
+                //black_box(File::create("vec.npy").unwrap());
+                let writer = File::create("vec.npy").unwrap();
+                vecs[i].write_npy(writer).unwrap();
+            })
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(benches, write_vec);
+criterion_main!(benches);
